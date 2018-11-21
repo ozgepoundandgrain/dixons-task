d8c18fb72a6853c678bc531c021390febcb89cbf
Tayeb Melksari <Tayeb.Melksari@dixonscarphone.com>
2018-11-21 15:41:45 +0000

    proposal in Issue #6. polyfill from config.js

--- src/example/config.js
+++ src/example/config.js
@@ -1,4 +1,8 @@
 export const config = {
-  activityName: '',
-  url: 'https://www.currys.co.uk/gbuk/index.html'
+  activityName: 'Example', // Optional
+  url: 'https://www.currys.co.uk/gbuk/index.html',
+  // port: 8080, // Optional
+  polyfill: [
+    // 'core-js/fn/promise' // Optional (Please make sure polyfills are installed)
+  ]
 };
